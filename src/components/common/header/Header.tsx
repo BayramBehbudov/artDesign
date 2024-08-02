c2eec02c498fc84539cc5bdba7446c34fdb93b2a
BayramBehbudov <bayram.behbudov@gmail.com>
2024-08-02 16:59:59 +0400

    started header

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,7 +1,7 @@
 interface IHeader {}
 
 const Header: React.FC = (): JSX.Element => {
-   return <header>Header</header>;
+   return <header className="container flex h-20 items-center bg-[#212529] text-white">header</header>;
 };
 
 export default Header;
